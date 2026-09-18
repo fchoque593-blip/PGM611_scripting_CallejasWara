ba15aa4e74ef2335a6de6c8bae2fde05d9582215
Wara Callejas <fchoque593@gmail.com>
2026-09-18 00:39:58 -0400

    Trabajo scripting

---
+++ Assets/scripts/herramientas.cs
@@ -0,0 +1,44 @@
+using herramientas.calculos;
+using herramientas.conectividad;
+using logica_de_jugador;
+using UnityEngine;
+
+namespace logica_de_jugador {
+    public class Jugador : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
+public class Enemigo
+{
+
+}
+
+#region Mundo
+namespace herramientas {
+    namespace calculos {
+        public class Ejemplo {
+            public void metodoEjemplo() {
+ 
+                Jugador miJugador; 
+            }
+        }
+    }
+    namespace conectividad {
+        public partial class Herramienta { 
+            
+        }
+    }
+}
+#endregion
