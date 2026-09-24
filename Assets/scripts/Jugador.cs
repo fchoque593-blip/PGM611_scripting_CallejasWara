4b7075d2540386595634175f021c94741a5fa531
Wara Callejas <fchoque593@gmail.com>
2026-09-24 19:02:24 -0400

    Agregar nuevas funcionalidades del jugador

--- Assets/scripts/Jugador.cs
+++ Assets/scripts/Jugador.cs
@@ -6,9 +6,17 @@ public class Jugador : MonoBehaviour
     public float velocidad = 5f;
     private Rigidbody2D rb;
     private float movimiento;
+    public float alturaSalto = 4f;
+    private bool esPiso;//true=estamos en el piso, false=estamos en el aire
+    public Transform comprobadorPiso;
+    public float radioComprobadorPiso = 0.1f;
+    public LayerMask layerPiso;
+
+    private Animator animator;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -17,5 +25,14 @@ public class Jugador : MonoBehaviour
         movimiento = Input.GetAxisRaw("Horizontal");
         rb.linearVelocity = new Vector2(movimiento * velocidad, rb.linearVelocity.y);
         if (movimiento!=0) transform.localScale = new Vector3(Mathf.Sign(movimiento),1,1);
+        if(Input.GetButtonDown("Jump")&&esPiso)
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, alturaSalto);
+        animator.SetFloat("Velocidad", Mathf.Abs(movimiento));
+        animator.SetFloat("VelocidadVertical", rb.linearVelocity.y);
+        animator.SetBool("estaEnPiso", esPiso);
+    }
+    public void FixedUpdate()
+    {
+        esPiso = Physics2D.OverlapCircle(comprobadorPiso.position, radioComprobadorPiso, layerPiso);
     }
 }
