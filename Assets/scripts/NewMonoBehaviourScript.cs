b710f987099a03ae4b59db06121e67a5d5180323
Wara Callejas <fchoque593@gmail.com>
2026-09-24 16:14:41 -0400

    Cambios y avances 3

---
+++ Assets/scripts/NewMonoBehaviourScript.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+namespace herramientas.conectividad {
+    public partial class Herramienta
+    {
+        public int contador;
+        public int vida;
+        
+        public int calculoPuntaje()
+        {
+     
+            return 0; 
+        }
+    }
+}
